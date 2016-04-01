2c9f6fc858bb4944cc76d8019bb270f96c13c419
amlinden <amlinden@kth.se>
2016-04-01 18:49:56 +0200

    zoomcontrolers, modified plancontroled
    pancontrol: fixed so that UP actually was up and not down and the same
    with left and right, modified the code to a shorter code and put them
    in wrappers so we did not need all the 4 divs, now just one

--- js/panControl.js
+++ js/panControl.js
@@ -1,59 +1,43 @@
-function PanningControl(panningControlDiv1,panningControlDiv2, panningControlDiv3,panningControlDiv4, map) {
+function PanningControl(controlDiv, map) {
 
-        // Set CSS for the UP control border.
-        var controlUp = document.createElement('div');
-        controlUp.style.backgroundColor = '#fff';
-        controlUp.style.border = '2px solid #fff';
-        controlUp.style.borderRadius = '3px';
-        controlUp.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-        controlUp.style.cursor = 'pointer';
-        controlUp.style.marginBottom = '22px';
-        controlUp.style.textAlign = 'center';
-        panningControlDiv1.appendChild(controlUp);
+
+        var controlWrapper = document.createElement('div');
+          controlWrapper.style.backgroundColor = 'white';
+          controlWrapper.style.borderStyle = 'solid';
+          controlWrapper.style.borderRadius = '10px';
+          controlWrapper.style.borderColor = 'gray';
+          controlWrapper.style.borderWidth = '1px';
+          controlWrapper.style.cursor = 'pointer';
+          controlWrapper.style.textAlign = 'center';
+          controlWrapper.style.width = '32px'; 
+  controlWrapper.style.height = '40px';
+          controlDiv.appendChild(controlWrapper);
+
+        var controlWrapper1 = document.createElement('div');
+          controlWrapper1.style.backgroundColor = 'white';
+          controlWrapper1.style.borderStyle = 'solid';
+          controlWrapper1.style.borderRadius = '10px';
+          controlWrapper1.style.borderColor = 'gray';
+          controlWrapper1.style.borderWidth = '1px';
+          controlWrapper1.style.cursor = 'pointer';
+          controlWrapper1.style.textAlign = 'center';
+          controlWrapper.style.width = '32px'; 
+  controlWrapper.style.height = '64px';
+          controlDiv.appendChild(controlWrapper1);
+     
 
         // Set CSS for the UP control interior.
         var controlTextUp = document.createElement('div');
         controlTextUp.style.color = 'rgb(25,25,25)';
         controlTextUp.style.fontFamily = 'Roboto,Arial,sans-serif';
-        controlTextUp.style.fontSize = '20px';
+        controlTextUp.style.fontSize = '15px';
         controlTextUp.style.lineHeight = 'automatic';
         controlTextUp.style.paddingLeft = '5px';
         controlTextUp.style.paddingRight = '5px';
         controlTextUp.innerHTML = 'Λ';
-        controlUp.appendChild(controlTextUp);
-
-        // Set CSS for the DOWN control border.
-        var controlDown = document.createElement('div');
-        controlDown.style.backgroundColor = '#fff';
-        controlDown.style.border = '2px solid #fff';
-        controlDown.style.borderRadius = '3px';
-        controlDown.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-        controlDown.style.cursor = 'pointer';
-        controlDown.style.marginBottom = '22px';
-        controlDown.style.textAlign = 'center';
-        panningControlDiv2.appendChild(controlDown);
-
-        // Set CSS for the DOWN control interior.
-        var controlTextDown = document.createElement('div');
-        controlTextDown.style.color = 'rgb(25,25,25)';
-        controlTextDown.style.fontFamily = 'Roboto,Arial,sans-serif';
-        controlTextDown.style.fontSize = '20px';
-        controlTextDown.style.lineHeight = 'automatic';
-        controlTextDown.style.paddingLeft = '5px';
-        controlTextDown.style.paddingRight = '5px';
-        controlTextDown.innerHTML = 'V';
-        controlDown.appendChild(controlTextDown);
-
+        controlWrapper.appendChild(controlTextUp);
         // Set CSS for the LEFT control border.
-        var controlLeft = document.createElement('div');
-        controlLeft.style.backgroundColor = '#fff';
-        controlLeft.style.border = '2px solid #fff';
-        controlLeft.style.borderRadius = '3px';
-        controlLeft.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-        controlLeft.style.cursor = 'pointer';
-        controlLeft.style.marginBottom = '22px';
-        controlLeft.style.textAlign = 'center';
-        panningControlDiv3.appendChild(controlLeft);
+        
 
         // Set CSS for the LEFT control interior.
         var controlTextLeft = document.createElement('div');
@@ -61,21 +45,12 @@ function PanningControl(panningControlDiv1,panningControlDiv2, panningControlDiv
         controlTextLeft.style.fontFamily = 'Roboto,Arial,sans-serif';
         controlTextLeft.style.fontSize = '20px';
         controlTextLeft.style.lineHeight = 'automatic';
-        controlTextLeft.style.paddingLeft = '5px';
+        controlTextLeft.style.paddingLeft = '0px';
         controlTextLeft.style.paddingRight = '5px';
         controlTextLeft.innerHTML = '<';
-        controlLeft.appendChild(controlTextLeft);
-
+        controlWrapper1.appendChild(controlTextLeft);
         // Set CSS for the UP control border.
-        var controlRight = document.createElement('div');
-        controlRight.style.backgroundColor = '#fff';
-        controlRight.style.border = '2px solid #fff';
-        controlRight.style.borderRadius = '3px';
-        controlRight.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-        controlRight.style.cursor = 'pointer';
-        controlRight.style.marginBottom = '22px';
-        controlRight.style.textAlign = 'center';
-        panningControlDiv4.appendChild(controlRight);
+     
 
         // Set CSS for the UP control interior.
         var controlTextRight = document.createElement('div');
@@ -84,33 +59,63 @@ function PanningControl(panningControlDiv1,panningControlDiv2, panningControlDiv
         controlTextRight.style.fontSize = '20px';
         controlTextRight.style.lineHeight = 'automatic';
         controlTextRight.style.paddingLeft = '5px';
-        controlTextRight.style.paddingRight = '5px';
+        controlTextRight.style.paddingRight = '0px';
         controlTextRight.innerHTML = '>';
-        controlRight.appendChild(controlTextRight);
+        controlWrapper1.appendChild(controlTextRight);
+
+        // Set CSS for the DOWN control border.
+       
+
+        // Set CSS for the DOWN control interior.
+        var controlTextDown = document.createElement('div');
+        controlTextDown.style.color = 'rgb(25,25,25)';
+        controlTextDown.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlTextDown.style.fontSize = '15px';
+        controlTextDown.style.lineHeight = 'automatic';
+        controlTextDown.style.paddingLeft = '5px';
+        controlTextDown.style.paddingRight = '5px';
+        controlTextDown.innerHTML = 'V';
+        controlWrapper.appendChild(controlTextDown);
+
+        
+
+        
 
         // Setup the click event listeners: pan up.
-        controlUp.addEventListener('click', function() {
-         var center = map.getCenter(); 
-         var New_center = new google.maps.LatLng(center.lat() - 0.01, center.lng()); 
-         map.setCenter(New_center);
-         });
-        // Setup the click event listeners: pan down.
-        controlDown.addEventListener('click', function() {
+        controlTextUp.addEventListener('click', function() {
          var center = map.getCenter(); 
          var New_center = new google.maps.LatLng(center.lat() + 0.01, center.lng()); 
          map.setCenter(New_center);
          });
+        // Setup the click event listeners: pan down.
+        controlTextDown.addEventListener('click', function() {
+         
+
+         var center = map.getCenter(); 
+         var New_center = new google.maps.LatLng(center.lat() - 0.01, center.lng()); 
+         map.setCenter(New_center);
+         });
         // Setup the click event listeners: pan left.
-        controlLeft.addEventListener('click', function() {
+        controlTextLeft.addEventListener('click', function() {
+
+            var center = map.getCenter(); 
+         var New_center = new google.maps.LatLng(center.lat(), center.lng() - 0.01); 
+         map.setCenter(New_center);
+
+         
+         });
+        // Setup the click event listeners: pan right.
+        controlTextRight.addEventListener('click', function() {
          var center = map.getCenter(); 
          var New_center = new google.maps.LatLng(center.lat(), center.lng() + 0.01); 
          map.setCenter(New_center);
          });
-        // Setup the click event listeners: pan right.
-        controlRight.addEventListener('click', function() {
-         var center = map.getCenter(); 
-         var New_center = new google.maps.LatLng(center.lat(), center.lng() - 0.01); 
-         map.setCenter(New_center);
-         });
 
       }
+
+
+
+
+
+
+
---
+++ js/zoomControl.js
@@ -0,0 +1,45 @@
+function ZoomControl (controlDiv, map){
+
+	controlDiv.style.padding = '5px';
+
+  // Set CSS for the control wrapper
+  var controlWrapper = document.createElement('div');
+  controlWrapper.style.backgroundColor = 'white';
+  controlWrapper.style.borderStyle = 'solid';
+  controlWrapper.style.borderRadius = '10px';
+  controlWrapper.style.borderColor = 'gray';
+  controlWrapper.style.borderWidth = '1px';
+  controlWrapper.style.cursor = 'pointer';
+  controlWrapper.style.textAlign = 'center';
+  controlWrapper.style.width = '32px'; 
+  controlWrapper.style.height = '64px';
+  controlDiv.appendChild(controlWrapper);
+
+  var zoomInButton = document.createElement('div');
+        zoomInButton.style.color = 'rgb(25,25,25)';
+        zoomInButton.style.fontFamily = 'Roboto,Arial,sans-serif';
+        zoomInButton.style.fontSize = '20px';
+        zoomInButton.style.lineHeight = 'automatic';
+        zoomInButton.style.paddingLeft = '5px';
+        zoomInButton.style.paddingRight = '5px';
+        zoomInButton.innerHTML = '+';
+        controlWrapper.appendChild(zoomInButton);
+   var zoomOutButton = document.createElement('div');
+        zoomOutButton.style.color = 'rgb(25,25,25)';
+        zoomOutButton.style.fontFamily = 'Roboto,Arial,sans-serif';
+        zoomOutButton.style.fontSize = '20px';
+        zoomOutButton.style.lineHeight = 'automatic';
+        zoomOutButton.style.paddingLeft = '5px';
+        zoomOutButton.style.paddingRight = '5px';
+        zoomOutButton.innerHTML = '_';
+        controlWrapper.appendChild(zoomOutButton);
+     // Setup the click event listener - zoomIn
+  google.maps.event.addDomListener(zoomInButton, 'click', function() {
+    map.setZoom(map.getZoom() + 1);
+  });
+
+  // Setup the click event listener - zoomOut
+  google.maps.event.addDomListener(zoomOutButton, 'click', function() {
+    map.setZoom(map.getZoom() - 1);
+  });  
+}
